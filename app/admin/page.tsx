c8ef885e7ee2e4b1bde59cf9c0f57bbafa7e859e
badrinarayanms <137931415+badrinarayanms@users.noreply.github.com>
2025-05-14 18:08:22 +0530

    Update page.tsx

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -135,7 +135,7 @@ export default function AdminDashboard() {
 
   return (
     <div className="space-y-4 p-32">
-       <Navbar />
+       
       <h2 className="text-xl font-bold text-black">Manage Country Clicks</h2>
       <div className="rounded-lg border border-purple-800 bg-purple-950/80 p-4">
         <div className="flex flex-col gap-4 sm:flex-row">
